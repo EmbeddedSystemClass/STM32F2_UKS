c513b461d84286bd597316548f515172eee79d61
nowhard <nowhard@mail.ru>
2015-01-15 14:41:59 +0300

    Начало разработки функции прогнозирования времени завершения процесса

---
+++ time_forecast/time_forecast.c
@@ -0,0 +1,6 @@
+#include "time_forecast.h"
+
+uint16_t Time_Forecast_Get_Time(float current_temp,float delta_temp)
+{
+
+}
---
+++ time_forecast/time_forecast.h
@@ -0,0 +1,4 @@
+#ifndef TIME_FORECAST_H
+#define TIME_FORECAST_H
+
+#endif
