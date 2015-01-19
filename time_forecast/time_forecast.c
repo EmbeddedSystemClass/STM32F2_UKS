b8159167cf40b9dc88d812d9595c2d907df3ee5b
nowhard <nowhard@mail.ru>
2015-01-19 16:16:13 +0300

    Начало разработки функции прогноза времени

--- time_forecast/time_forecast.c
+++ time_forecast/time_forecast.c
@@ -1,6 +1,9 @@
 #include "time_forecast.h"
 
-uint16_t Time_Forecast_Get_Time(float current_temp,float delta_temp)
+#define MAX_FORECAST_TIME			30//MINUTES
+#define NUM_OF_CHARACTERISTIC		5
+uint16_t Time_Forecast_Get_Time(float delta_temp,uint16_t current_time)
 {
 
+	return 0;
 }
